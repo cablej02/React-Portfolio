b1ea751ffd64001c45fd02d80e3919b1403c846f
Jason Cable <cablej02@gmail.com>
2024-12-17 16:31:19 -0600

    added react router dependency fand cleaned up duplicate files

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-
+//import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.css'
+
 import App from './App.jsx'
+import Error from './pages/error.jsx'
 import About from './pages/about.jsx'
 import Portfolio from './pages/portfolio.jsx';
 import Resume from './pages/resume.jsx';
