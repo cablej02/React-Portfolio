7a9d6c95313427ea7d971b09ec6cd07d2025d766
Jason Cable <cablej02@gmail.com>
2024-12-18 17:02:46 -0600

    added skills grid to about

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-// import './index.css'
+import './index.css'
 // import './App.css'
 
 import App from './App.jsx'
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,22 +1,20 @@
 export default function About() {
+    const skills = [
+        "HTML", "CSS", "JavaScript", "React", "Node.js", "Express.js", 
+        "SQL", "Python", "C#", "Tableau", "Power BI", "Unity"
+    ];
+
     return (
         <div className="container">
             <img src="/src/assets/myAvatar.png" alt="avatar" style={{ width: '100px', height: '100px' }} />
             <p>I am a full-stack developer, game developer, and business intelligence professional. My skills include: </p>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Node.js</li>
-                <li>Express.js</li>
-                <li>SQL</li>
-                <li>Python</li>
-                <li>C#</li>
-                <li>Tableau</li>
-                <li>Power BI</li>
-                <li>Unity</li>
-            </ul>
+            <div className="skills-grid">
+                {skills.map(skill => (
+                    <div className="skill-card" key={skill}>
+                        {skill}
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
