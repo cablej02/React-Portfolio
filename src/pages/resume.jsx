bb6cd75968f4be629c561be585b34929de3987ff
Jason Cable <cablej02@gmail.com>
2024-12-18 22:08:30 -0600

    added resume content

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,8 +1,33 @@
 export default function Resume() {
     return (
-        <div>
-            <h1>Resume</h1>
-            <p>This is the resume page</p>
+        <div className="container pb-5">
+            <h2 className="display-4 fw-normal mb-4">Job Experience</h2>
+            <div className="card">
+                <h3>Mobile Game Designer and Developer with Wayward Gaming</h3>
+                <p>Designed, developed, and published games to mobile marketplaces using primarily Unity and C#.</p>
+                <p><strong>Duration:</strong> Jan 2019 - Present</p>
+            </div>
+            <div className="card">
+                <h3>Business Intelligence Developer with TASC</h3>
+                <p>Developed and maintained reports and visualizations using primarily SQL, Power BI, and Tableu.  Created and maintained database for internal audit processes. </p>
+                <p><strong>Duration:</strong> Jan 2017 - July 2020</p>
+            </div>
+            <h2 className="display-4 fw-normal">Education</h2>
+            <div className="card">
+                <h3>Full Stack Web Development Certificate from the University of Wisconsin Extended Campus and edX</h3>
+                <p>In the process of completing the Full Stack Web Development bootcamp expected to complete in March 2025.</p>
+                <p><strong>Duration:</strong> 2024-preset</p>
+            </div>
+            <div className="card">
+                <h3>University of Wisconsin - Oshkosh</h3>
+                <p>Studied music performance with an emphasis on violin.</p>
+                <p><strong>Duration:</strong> 2008-2013</p>
+            </div>
+            <div className="card">
+                <h3>University of Wisconsin - Madison</h3>
+                <p>General Studies</p>
+                <p><strong>Duration:</strong> 2006-2008</p>
+            </div>
         </div>
     )
 }
