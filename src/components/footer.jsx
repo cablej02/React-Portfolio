bb73b885a717902fb637d444b3e3ae52c3e5379b
Jason Cable <cablej02@gmail.com>
2024-12-20 00:15:49 -0600

    added more styling and footer content

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,15 @@
+import { FaFacebook, FaTwitter, FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
+
 export default function Footer() {
     return (
-        <footer className="py-3" style={{ position: 'fixed', bottom: 0, left: 0, width: '100%', backgroundColor: 'grey', zIndex: 1000 }}>
-            <div className="container" style={{ textAlign: 'center' }}>
-                {/* TODO: Add GitHub and LinkedIn links here */}
-                <p>Placeholder footer</p>
+        <footer className="py-3" style={{ position: 'fixed', bottom: 0, left: 0, width: '100%', zIndex: 1000 }}>
+            <div className="container d-flex justify-content-center">
+                <a href="https://www.github.com/cablej02" target="_blank" className="mx-2 noopener noreferrer">
+                    <FaGithub size={30} />
+                </a>
+                <a href="https://linkedin.com/" target="_blank" className="mx-2 noopener noreferrer">
+                    <FaLinkedin size={30} />
+                </a>
             </div>
         </footer>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 //TODO - cleanup css imports to only what's needed
 import 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.css'
 // import './App.css'
 
