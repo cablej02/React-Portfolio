e5f504e6bddfcbf95592d112c06a2cbc30c58907
Jason Cable <cablej02@gmail.com>
2024-12-20 01:14:31 -0600

    added contact form

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-//TODO - cleanup css imports to only what's needed
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './index.css'
-// import './App.css'
 
 import App from './App.jsx'
 import Error from './pages/error.jsx'
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,8 +1,26 @@
 export default function Contact() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData.entries());
+        //TODO: send data to server
+        console.log(data);
+    }
+
     return (
         <div>
-            <h1>Contact</h1>
-            <p>This is the contact page</p>
+            <h2>Contact Me</h2>
+            <form className="contact-form" style={{display:'flex', flexDirection:'column', width: '400px'}} onSubmit={handleSubmit}>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="name" required />
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+                <label htmlFor="message">Message:</label>
+                <textarea id="message" name="message" required style={{height: '200px'}}>
+                </textarea>
+                <button type="submit" className="submit-button">Send</button>
+            </form>
         </div>
     )
 }
